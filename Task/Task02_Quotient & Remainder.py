71424fbb196452ed2dcb5e62da587eb5ec2167a9
NagaPhani333 <nagaphaneendra.allam@gmail.com>
2025-10-16 06:58:03 +0530

    Task2 and 3 changes and task3

---
+++ Task/Task02_Quotient & Remainder.py
@@ -0,0 +1,9 @@
+#Find remainder and Quotient
+Num1 = int(input("Enter number 1 : "))
+Num2 = int(input("Enter number 2 : "))
+Num3 = Num1//Num2
+Num4 = Num1%Num2
+Num5 = Num1/Num2
+print("quotient is Num3 : ", Num3)
+print("remainder is Num4 : ", Num4)
+print("remainder is Num5: ", Num5)
--- Task/Task03_area of circle.py
+++ Task/Task03_area of circle.py
@@ -1,7 +1,11 @@
+import math
+
 radius = float(input("Please enter radius : ")) #116
 print("Radius of circle is : ", radius)
-area = 3.14*(radius**2)
-area1 = 3.14*pow(radius, 2)
+area = 3.14*(radius**2) #using operators
+area1 = 3.14*pow(radius, 2) #using pow function
+area2 = math.pi* pow(radius, 2) #using math functions
 print("Area of circle is : ", str(area))
-print(f"Area of circle is  -> using str data formating : {area:.1f}" )
+print(f"Area of circle is  -> using str data formating : {area:.3f}" )
 print(f"Area of circle is  -> using str data formating : {area1:.1f}")
+print(f"Area of circle is  -> using str data formating : {area2:.2f}")
