0dc3b6a7d4424b73e5a6120bb6b64eebea392930
NagaPhani333 <nagaphaneendra.allam@gmail.com>
2025-10-16 07:04:19 +0530

    Tas 1 changes and task3

---
+++ Task/Task01_Add_Mul_Sub_Divide.py
@@ -0,0 +1,13 @@
+#Find remainder and Quotient
+Num1 = int(input("Enter number 1 : "))
+Num2 = int(input("Enter number 2 : "))
+Num6 = int(input("Enter number 3 : "))
+Num3 = Num1+Num2+Num6
+Num4 = Num1-Num2
+Num5 = Num1/Num2
+Num7 = Num2*Num6
+Num8 = Num1*Num3+Num2/Num6
+print("Sum is Num3 : ", Num3)
+print("Sub is Num4 : ", Num4)
+print("Div is Num5: ", Num5)
+print("Mul is Num7: ", Num7)
